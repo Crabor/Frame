fdf5e93640bc68868abbffdc1868fbe9c9c05c62
crabor <crabor19711211@163.com>
2022-03-14 18:34:00 +0800

    update

--- src/main/java/frame/resource/actor/Actor.java
+++ src/main/java/frame/resource/actor/Actor.java
@@ -1,17 +1,5 @@
 package frame.resource.actor;
 
 public class Actor {
-    public Dir dir;
-    public Double speed;
-    public Double time;
 
-    public Actor(Dir dir, Double speed, Double time) {
-        this.dir = dir;
-        this.speed = speed;
-        this.time = time;
-    }
-
-    public Actor(String dir, String speed, String time) {
-        this(Dir.valueOf(dir.toUpperCase()), Double.valueOf(speed), Double.valueOf(time));
-    }
 }
--- src/main/java/frame/resource/sensor/Sensor.java
+++ src/main/java/frame/resource/sensor/Sensor.java
@@ -1,17 +1,5 @@
 package frame.resource.sensor;
 
 public class Sensor {
-    public String name;
-    public Type type;
-    public Double value;
 
-    public Sensor(String name, Type type, Double value) {
-        this.name = name;
-        this.type = type;
-        this.value = value;
-    }
-
-    public Sensor(String name, String type, String value) {
-        this(name, Type.valueOf(type.toUpperCase()), Double.valueOf(value));
-    }
 }
