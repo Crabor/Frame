eb1291d6c47c2a304bf8fb65a7255ef2c7dbf82d
Crabor <crabor19711211@163.com>
2022-04-14 13:45:18 +0800

    update

--- src/main/java/platform/resource/driver/UIDriver.java
+++ src/main/java/platform/resource/driver/UIDriver.java
@@ -15,7 +15,7 @@ public class UIDriver extends AbstractSubscriber implements Runnable {
     private int clientPort;
 
     public UIDriver(int serverPort, String clientAddress, int clientPort) {
-        // TODO 貌似不会调用Abstract的构造函数
+        // TODO
         this.serverPort = serverPort;
         this.clientPort = clientPort;
         try {
