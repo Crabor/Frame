f1da9f571c4e5669e016680abb14de6c18be237f
Lingyu Zhang <dz21330043@smail.nju.edu.cn>
2022-10-18 15:16:41 +0800

    merge ctx-refactor

--- test/main/java/platform/testTaxi/demo.java
+++ test/main/java/platform/testTaxi/demo.java
@@ -5,6 +5,6 @@ import platform.Platform;
 public class demo {
     public static void main(String[] args) {
         Platform.Init();
-        Platform.Start();
+        Platform.Start();  
     }
 }
