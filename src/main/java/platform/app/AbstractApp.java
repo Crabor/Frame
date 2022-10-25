bf3c7a9973f8cccad2a4ad616bf6ae9eff365d79
Crabor <Uestc617>
2022-10-25 21:08:39 +0800

    app注册/取消注册sensor添加进Configuration

--- src/main/java/platform/app/AbstractApp.java
+++ src/main/java/platform/app/AbstractApp.java
@@ -1,8 +1,11 @@
 package platform.app;
 
+import platform.config.Configuration;
 import platform.pubsub.AbstractSubscriber;
 import platform.service.ctx.ctxServer.CtxInteractor;
 import platform.service.inv.CancerServer;
+
+import java.util.Set;
 
 public abstract class AbstractApp extends AbstractSubscriber implements App{
     protected String appName;
@@ -29,4 +32,30 @@ public abstract class AbstractApp extends AbstractSubscriber implements App{
     }
 
     protected abstract void customizeCtxServer();
+
+    public static void registerSensor(String appName, Set<String> sensorNames) {
+        try {
+            Configuration.getAppsConfig().get(appName).addSensors(sensorNames);
+            sensorNames.forEach(s -> Configuration.getResourceConfig().getSensorsConfig().get(s).addApp(appName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerSensor(String... sensors) {
+        registerSensor(appName, Set.of(sensors));
+    }
+
+    public static void cancelSensor(String appName, Set<String> sensorNames) {
+        try {
+            Configuration.getAppsConfig().get(appName).removeSensors(sensorNames);
+            sensorNames.forEach(s -> Configuration.getResourceConfig().getSensorsConfig().get(s).removeApp(appName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cancelSensor(String... sensors) {
+        cancelSensor(appName, Set.of(sensors));
+    }
 }
