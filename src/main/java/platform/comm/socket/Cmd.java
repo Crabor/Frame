bda49c405d1f77a3349602aa2e955f5a1dced307
Crabor <Uestc617>
2022-11-07 17:32:39 +0800

    资源管理version2 ok

--- src/main/java/platform/comm/socket/Cmd.java
+++ src/main/java/platform/comm/socket/Cmd.java
@@ -16,6 +16,7 @@ public class Cmd {
         UDP.send(Util.formatCommand(cmd, args));
     }
 
+    //此方法只允许在DeviceDriver类的run方法中调用，望周知
     public static CmdRet recv() {
         return Util.decodeCommandRet(UDP.recv());
     }
