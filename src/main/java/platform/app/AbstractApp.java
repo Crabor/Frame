4a0b4953ad453df7aad105ffd523ebff282bf96e
Crabor <Uestc617>
2022-11-12 17:02:50 +0800

    fix invsever off bug

--- src/main/java/platform/app/AbstractApp.java
+++ src/main/java/platform/app/AbstractApp.java
@@ -24,9 +24,10 @@ public abstract class AbstractApp extends AbstractSubscriber implements App, Run
     @Override
     public void onMessage(String channel, String msg) {
         iterId++;
-        CancerServer.iterEntry(appName, iterId, msg);
+        boolean invSeverOn = Configuration.getInvServerConfig().isServerOn();
+        if (invSeverOn) CancerServer.iterEntry(appName, iterId, msg);
         iter(channel, msg);
-        CancerServer.iterExit(appName, iterId);
+        if (invSeverOn) CancerServer.iterExit(appName, iterId);
     }
 
     @Override
