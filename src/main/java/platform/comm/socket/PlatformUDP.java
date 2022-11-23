f7e235290ffc3d67f43489733b2ba77dadea58df
Crabor <Uestc617>
2022-11-23 19:52:31 +0800

    PlatformUDP发送消息到多个端口

--- src/main/java/platform/comm/socket/PlatformUDP.java
+++ src/main/java/platform/comm/socket/PlatformUDP.java
@@ -10,7 +10,9 @@ import java.net.UnknownHostException;
 
 public class PlatformUDP {
     public static void send(Cmd cmd) {
-        UDP.send("255.255.255.255", Configuration.getUdpConfig().getClientPort(), cmd.toJSONString());
+        for (int port : Configuration.getUdpConfig().getClientPort()) {
+            UDP.send("255.255.255.255", port, cmd.toJSONString());
+        }
     }
 
     //此方法只允许在DeviceDriver类的run方法中调用，望周知
